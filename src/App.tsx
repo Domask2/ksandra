205641aaf857f2c9566f91ea2d2cbc4cd42ac052
Гордеев Сергей <domask1989@gmail.com>
2022-08-18 12:37:06 +0300

    add: Header/Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-  w import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {Button, Checkbox, Form, Input, Modal} from "antd";
+import {LoginOutlined} from '@ant-design/icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [isModalVisible, setIsModalVisible] = React.useState<boolean>(false);
+
+    const handleOk = () => {
+        setIsModalVisible(false)
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    const onFinish = (values: any) => {
+        console.log(values)
+        setIsModalVisible(false)
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo)
+    };
+
+    const auth = {authenticated: false};
+
+    return (
+        <>
+            <header className="lcHeader" style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                alignContent: 'center',
+                padding: '10px 20px',
+                minHeight: '70px'
+            }}>
+
+                <div style={{right: '0px'}} onClick={() => setIsModalVisible(true)}>
+                    <LoginOutlined style={{marginRight: '5px'}}/>
+                    {auth.authenticated ? 'Выйти' : 'Войти в систему'}
+                </div>
+
+                <Modal title="Авторизация" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}
+                       footer={false}>
+                    <Form
+                        name="basic"
+                        labelCol={{span: 8}}
+                        wrapperCol={{span: 16}}
+                        initialValues={{remember: true}}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                    >
+                        <Form.Item
+                            label="Email"
+                            name="email"
+                            rules={[{required: true, message: 'Please input your username!'}]}
+                        >
+                            <Input/>
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Пароль"
+                            name="password"
+                            rules={[{required: true, message: 'Please input your password!'}]}
+                        >
+                            <Input.Password/>
+                        </Form.Item>
+
+                        <Form.Item name="remember" valuePropName="checked" wrapperCol={{offset: 8, span: 16}}>
+                            <Checkbox>Запомнить</Checkbox>
+                        </Form.Item>
+
+                        <Form.Item wrapperCol={{offset: 8, span: 16}}>
+                            <Button type="primary" htmlType="submit">
+                                Войти
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Modal>
+
+            </header>
+
+        </>
+    )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import "antd/dist/antd.css";
+import "./styles/index.css";
+import "./styles/App.css";
+import "./styles/Print.css";
+import "./styles/my_ant.css";
+import "./styles/labels.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +19,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
